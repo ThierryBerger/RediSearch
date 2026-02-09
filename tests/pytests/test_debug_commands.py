470139c76da312be67d81b26afd10f11b3956cd8
lerman25 <lerman25@gmail.com>
2026-02-09 22:12:18 +0200

    disable debug timeout from shard policy not return

--- tests/pytests/test_debug_commands.py
+++ tests/pytests/test_debug_commands.py
@@ -783,8 +783,12 @@ class TestQueryDebugCommands(object):
     def StrictPolicy(self):
         env = self.env
         env.expect(config_cmd(), 'SET', 'ON_TIMEOUT', 'FAIL').ok()
+        if env.shardsCount > 1:
+            err_msg = "Timeout limit was reached"
+        else:
+            err_msg = "TIMEOUT_AFTER_N is only supported with ON_TIMEOUT RETURN"
 
-        with env.assertResponseError(contained="Timeout limit was reached"):
+        with env.assertResponseError(contained="TIMEOUT_AFTER_N is only supported with ON_TIMEOUT RETURN"):
             runDebugQueryCommandTimeoutAfterN(env, self.basic_query, 2)
 
         # restore the default policy
